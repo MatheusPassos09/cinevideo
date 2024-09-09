f0659bc44b7f4bbd86890ee6b975d5f692af52fa
Matheus <123427959+MatheusPassos09@users.noreply.github.com>
2024-09-09 15:22:45 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -2,21 +2,21 @@ let dados = [
     {
         titulo : "Vingadores",
         sinopse : "Os Vingadores marca um marco na história do cinema, reunindo pela primeira vez os maiores heróis da Marvel em um único filme épico. A trama se desenrola quando Loki, o ambicioso irmão de Thor, rouba o Tesseract e une forças com os Chitauri, uma raça alienígena, com o objetivo de dominar a Terra.",
-        link : "https://pt.wikipedia.org/wiki/Vingadores",
+        genero : "https://pt.wikipedia.org/wiki/Vingadores",
         tags : "marvel vingador heróis herois" 
     },
     
     {
         titulo : "The Last Of Us",
         sinopse : "Em um mundo devastado por um fungo que transforma humanos em criaturas monstruosas, Joel, um sobrevivente endurecido, é contratado para escoltar Ellie, uma adolescente imune à infecção, através de uma América pós-apocalíptica. Juntos, eles embarcam em uma jornada perigosa e emocional, enfrentando perigos tanto humanos quanto naturais.",
-        link : "https://pt.wikipedia.org/wiki/The_Last_of_Us_(série_de_televisão)",
+        genero : "https://pt.wikipedia.org/wiki/The_Last_of_Us_(série_de_televisão)",
         tags : "apocalipse zumbi criatura"
     },
 
     {
         titulo : "Stranger Things",
         sinopse : "Em uma pequena cidade americana dos anos 80, um garoto desaparece misteriosamente. Enquanto a polícia local e a mãe do menino buscam por respostas, eventos estranhos começam a ocorrer, incluindo a aparição de uma garota com poderes telecinéticos e experimentos secretos do governo envolvendo dimensões paralelas.",
-        link : "https://pt.wikipedia.org/wiki.org/Stranger_Things",
+        genero : "https://pt.wikipedia.org/wiki.org/Stranger_Things",
         tags : "mundo paralelo monstro dimensão"
 
 }
