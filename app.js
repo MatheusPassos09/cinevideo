1f1a6ae1ab5a4da7d6b2236ca4eea4727ff1ad61
Matheus <123427959+MatheusPassos09@users.noreply.github.com>
2024-09-09 13:04:28 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+function pesquisar() {
+   // Obtém a seção onde os resultados serão exibidos
+   let section = document.getElementById("resultados-pesquisa");
+
+   let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+   // se campoPesquisa for uma string sem nada
+   if (!campoPesquisa) {
+      section.innerHTML = "<p>Nada encontrado</p>"
+      return
+   }
+
+   campoPesquisa = campoPesquisa.toLowerCase()
+
+
+   // Inicializa a string que armazenará os resultados da pesquisa
+   let resultados = "";
+   let titulo = "";
+   let sinopse = "";
+   let tags = "";
+ 
+   // Itera sobre cada dado na lista de dados
+   for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        sinopse = dado.sinopse.toLowerCase()
+        tags = dado.tags.toLowerCase()
+     // se titulo includes campoPesquisa
+         if (titulo.includes(campoPesquisa) || sinopse.
+            includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // cria um novo elemento 
+         resultados += `
+         <div class="item-resultado">
+            <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
+             <p class="descricao-meta">${dado.sinopse}</p>
+             <a href=${dado.link} target="_blank">Mais informações</a>
+         </div>
+    `;   
+   }
+      
+ }
+
+   if (!resultados) {
+      resultados = "<p>Nada encontrado</p>"
+   }
+ 
+   // Atribui o HTML gerado à seção de resultados
+   section.innerHTML = resultados;
+ }
