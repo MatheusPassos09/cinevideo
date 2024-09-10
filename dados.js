c61b78bb83eb44e3e25af9c91f83a31ad2fbca45
Matheus <123427959+MatheusPassos09@users.noreply.github.com>
2024-09-09 23:34:30 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -2,24 +2,46 @@ let dados = [
     {
         titulo : "Vingadores",
         sinopse : "Os Vingadores marca um marco na história do cinema, reunindo pela primeira vez os maiores heróis da Marvel em um único filme épico. A trama se desenrola quando Loki, o ambicioso irmão de Thor, rouba o Tesseract e une forças com os Chitauri, uma raça alienígena, com o objetivo de dominar a Terra.",
-        genero : "https://pt.wikipedia.org/wiki/Vingadores",
+        genero : "Ação, Aventura, Ficção científica, Fantasia",
         tags : "marvel vingador heróis herois" 
     },
     
     {
         titulo : "The Last Of Us",
         sinopse : "Em um mundo devastado por um fungo que transforma humanos em criaturas monstruosas, Joel, um sobrevivente endurecido, é contratado para escoltar Ellie, uma adolescente imune à infecção, através de uma América pós-apocalíptica. Juntos, eles embarcam em uma jornada perigosa e emocional, enfrentando perigos tanto humanos quanto naturais.",
-        genero : "https://pt.wikipedia.org/wiki/The_Last_of_Us_(série_de_televisão)",
+        genero : "Suspense, Terror, Ficção pós-apocalíptica",
         tags : "apocalipse zumbi criatura"
     },
 
     {
         titulo : "Stranger Things",
         sinopse : "Em uma pequena cidade americana dos anos 80, um garoto desaparece misteriosamente. Enquanto a polícia local e a mãe do menino buscam por respostas, eventos estranhos começam a ocorrer, incluindo a aparição de uma garota com poderes telecinéticos e experimentos secretos do governo envolvendo dimensões paralelas.",
-        genero : "https://pt.wikipedia.org/wiki.org/Stranger_Things",
-        tags : "mundo paralelo monstro dimensão"
+        genero : "Sobrenatural, Mistério, Terror",
+        tags : "mundo paralelo monstro dimensão",
+    },
 
-}
+    {
+        titulo : "Homem-Aranha",
+        sinopse : "Picado por uma aranha radioativa, o tímido Peter Parker ganha incríveis superpoderes. Com a habilidade de escalar paredes, força sobre-humana e a capacidade de lançar teias, ele se transforma no lendário Homem-Aranha.",
+        genero : "Aventura, Ação, Fantasia, Herói",
+        tags : "#",
+    },
+
+    {
+        titulo : "O Senhor dos Anéis: Os Anéis de Poder",
+        sinopse : "Milhares de anos antes dos eventos de O Hobbit e O Senhor dos Anéis, a Terra-média'' era um lugar muito diferente. A série Os Anéis de Poder nos transporta para a Segunda Era, um período de grande beleza e perigos iminentes.",
+        genero : "Drama, Aventura, Fantasia",
+        tags : "aneis anéis",
+    },
+    {
+
+        titulo : "The Boys",
+        sinopse : "The Boys é uma série de televisão que subverte o gênero de super-heróis, apresentando uma visão cínica e violenta de um mundo onde indivíduos superpoderosos são celebridades manipuladas por uma corporação poderosa. A série explora os excessos, a corrupção e as consequências de ter super-heróis como ídolos.",
+        genero : "Ação, Humor ácido, Comédia",
+        tags : "#",
+    },
+
+
 ]
 
 
